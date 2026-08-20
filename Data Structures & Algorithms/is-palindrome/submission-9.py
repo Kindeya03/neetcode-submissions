05ccc776a43fd72ec4d1ac5c2ac8c7a6b0b06937
Kindeya Debesu <kindeyadebesu21@gmail.com>
2026-08-20 10:32:54 -0500

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,25 @@
+import string
+lowercase = string.ascii_lowercase
+uppercase = string.ascii_uppercase
+numbers = string.digits
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s)< 2:
+            return True
+        frwd = 0
+        back = len(s)-1
+        while back >= frwd:
+            while not s[frwd].isalnum():
+                if frwd >= len(s)-1:
+                    return True
+                frwd +=1
+            while not s[back].isalnum():
+                if back<= 0:
+                    return True
+                back -=1
+            # print(f'frwd ={s[frwd].lower()} [{frwd}] back = {s[back].lower()} [{back}]')
+            if s[frwd].lower() != s[back].lower():
+                return False
+            frwd +=1
+            back -=1
+        return True
